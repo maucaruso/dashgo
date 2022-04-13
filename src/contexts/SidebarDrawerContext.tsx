04c7cb3dd18dd40ce5408b589ed9a8bb88ef4e8b
maurici3 <carusojr@outlook.com>
2022-04-13 07:42:52 -0300

    chore: mobile adapt dashboard

---
+++ src/contexts/SidebarDrawerContext.tsx
@@ -0,0 +1,28 @@
+import { useDisclosure, UseDisclosureReturn } from "@chakra-ui/react";
+import { createContext, ReactNode, useContext, useEffect } from "react";
+import { useRouter } from "next/router";
+
+interface SidebartProviderProps {
+  children: ReactNode;
+}
+
+type SidebarDrawerContextData = UseDisclosureReturn;
+
+const SidebarDrawerContext = createContext({} as SidebarDrawerContextData);
+
+export function SidebarDrawerProvider({ children }: SidebartProviderProps) {
+  const disclosure = useDisclosure();
+  const router = useRouter();
+
+  useEffect(() => {
+    disclosure.onClose();
+  }, [router.asPath]);
+
+  return (
+    <SidebarDrawerContext.Provider value={disclosure}>
+      {children}
+    </SidebarDrawerContext.Provider>
+  );
+}
+
+export const useSidebarDrawer = () => useContext(SidebarDrawerContext);
